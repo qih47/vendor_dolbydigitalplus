1241805b6e41fd8432a280c79c8f7e022003a0bd
Qisthi Iskandar Haqiki <qisthih@gmail.com>
2020-08-22 13:50:43 +0700

    DDP : Fix path and add soundfx prebuilt

--- config.mk
+++ config.mk
@@ -1,26 +1,13 @@
-# Copyright (C) 2020 The PixelExperience Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (C) 2020 The Dolby Digital Plus Project
 
 VENDOR_PATH := vendor/DDPlus
 
 PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc) \
-
-PRODUCT_COPY_FILES += \
-	vendor/DDPlus/system/vendor/lib/libstagefright_soft_ac4dec.so:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib/libstagefright_soft_ac4dec.so \
-	vendor/DDPlus/system/vendor/lib/libstagefright_soft_ddpdec.so:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib/libstagefright_soft_ddpdec.so \
-	vendor/DDPlus/system/vendor/lib/soundfx/libdseffect.so:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib/soundfx/libdseffect.so \
+    vendor/DDPlus/system/etc/ds1-default.xml:$(TARGET_COPY_OUT_SYSTEM)/system/etc/ds1-default.xml \
+    vendor/DDPlus/system/etc/permissions/android.dolby.xml:$(TARGET_COPY_OUT_SYSTEM)/system/etc/permissions/android.dolby.xml \
+	vendor/DDPlus/system/vendor/lib/libstagefright_soft_ac4dec.so:$(TARGET_COPY_OUT_VENDOR)/vendor/lib/libstagefright_soft_ac4dec.so \
+	vendor/DDPlus/system/vendor/lib/libstagefright_soft_ddpdec.so:$(TARGET_COPY_OUT_VENDOR)/vendor/lib/libstagefright_soft_ddpdec.so \
+	vendor/DDPlus/system/vendor/lib/soundfx/libdseffect.so:$(TARGET_COPY_OUT_VENDOR)/vendor/lib/soundfx/libdseffect.so \
 	vendor/DDPlus/system/framework/dolby_ds.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/dolby_ds.jar 
   	
 
--- system/Android.mk
+++ system/Android.mk
@@ -1,1 +1,10 @@
-include $(call all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES         := vendor/lib/soundfx
+LOCAL_MODULE            := libdseffect.so
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := LIB
+LOCAL_SRC_FILES         := libdseffect.so
+LOCAL_MODULE_PATH       := $(TARGET_OUT)/vendor/lib/soundfx
+include $(BUILD_PREBUILT)
