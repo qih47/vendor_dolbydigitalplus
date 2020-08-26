fa91d18ac0ba538e28267d19d113d077dc9b22d6
Qisthi Iskandar Haqiki <35188882+qih47@users.noreply.github.com>
2020-08-26 22:58:43 +0700

    DDP: Nuke duplicate line

--- system/Android.mk
+++ system/Android.mk
@@ -1,10 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES         := vendor/lib/soundfx
 LOCAL_MODULE            := libdseffect.so
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := LIB
-LOCAL_SRC_FILES         := libdseffect.so
+LOCAL_SRC_FILES         := vendor/lib/soundfx/libdseffect.so
 LOCAL_MODULE_PATH       := $(TARGET_OUT)/vendor/lib/soundfx
 include $(BUILD_PREBUILT)
